494eb9229ff599b5739b229983a5d9f6bb484ce9
Jun Teng <Jun Teng@JUNTENG-NB>
2024-04-18 17:43:40 +0800

    Add project files.

---
+++ BarcodeReader/MainWindow.xaml.cs
@@ -0,0 +1,262 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO.Ports;
+using System.Net.Sockets;
+using System;
+using System.Net;
+using SmartCodeReader;
+
+namespace BarcodeReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SerialPort serialPort = new SerialPort();
+        string serverIP = "192.168.2.10";
+        int serverPort = 2005;
+        int commandPort = 4001;
+        CodeReader cr = new CodeReader();
+
+        // Create delegates
+        public delegate (bool, Exception?) SerialDelegate(SerialPort serialPort);
+        public delegate (string?, Exception?) SerialReceiveDelegate(SerialPort serialPort);
+
+        public delegate (bool, Exception?) TcpDelegate(string serverIP, int serverPort);
+        public delegate (string?, Exception?) TcpReceiveDelegate(string serverIP, int serverPort);
+
+        // Set up delegates
+        SerialDelegate? serialDelegate;
+        SerialReceiveDelegate? serialReceiveDelegate;
+
+        TcpDelegate? tcpDelegate;
+        TcpReceiveDelegate? tcpReceiveDelegate;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            pathCombo.Items.Add("RS232");
+            pathCombo.Items.Add("TCP");
+            portCombo.SelectedIndex = -1;
+        }
+
+        private async void btnConn_Click(object sender, RoutedEventArgs e) //Establish a connection to the port
+        {
+            try
+            {
+                if (pathCombo.SelectedItem.ToString() == "RS232")
+                {
+                    // Open port and start acquisition
+                    serialPort.BaudRate = 9600;
+                    serialPort.Parity = Parity.None;
+                    serialPort.DataBits = 8;
+                    serialPort.StopBits = StopBits.One;
+                    serialPort.Handshake = Handshake.None;
+                    serialPort.DtrEnable = true;
+                    serialPort.RtsEnable = true;
+                    serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                    serialPort.PortName = portCombo.Text;
+                    serialDelegate = cr.serialConnect;
+                    var con = serialDelegate(serialPort);
+                    if (con.Item1)
+                    {
+                        MessageBox.Show("The port is now opened successfully.");
+                    }
+                    else
+                    {
+                        if (con.Item2 != null)
+                        {
+                            MessageBox.Show(con.Item2.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+                else if(pathCombo.SelectedItem.ToString() == "TCP")
+                {
+                    // Add a loader here
+                    Dispatcher.Invoke(() => { txtLoad.Visibility = Visibility.Visible; });
+                    // Establish a TCP connection to start acquisition
+                    await Task.Run(() =>
+                    {
+                        tcpDelegate = cr.tcpConnect;
+                        var con = tcpDelegate(serverIP, commandPort);
+                        Dispatcher.Invoke(() => { txtLoad.Visibility = Visibility.Collapsed; });
+
+                        if (con.Item1)
+                        {
+                            MessageBox.Show("TCP connection is established successfully.");
+                        }
+                        else
+                        {
+                            if (con.Item2 != null)
+                            {
+                                MessageBox.Show(con.Item2.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
+                        }
+                    });
+                }
+            }
+            catch(Exception ex)
+            { 
+                MessageBox.Show(ex.Message,"Message",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnDisconn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (pathCombo.SelectedItem.ToString() == "RS232")
+                {
+                    // Stop acquisition and close port
+                    serialDelegate = cr.serialDisconnect;
+                    var con = serialDelegate(serialPort);
+                    if (con.Item1)
+                    {
+                        MessageBox.Show("The port is closed successfully.");
+                    }
+                    else
+                    {
+                        if (con.Item2 != null)
+                        {
+                            MessageBox.Show(con.Item2.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+                else if (pathCombo.SelectedItem.ToString() == "TCP")
+                {
+                    // Establish a TCP connection to stop acquisition
+                    tcpDelegate = cr.tcpDisconnect;
+                    var con = tcpDelegate(serverIP, commandPort);
+                    if (con.Item1)
+                    {
+                        MessageBox.Show("TCP connection is closed successfully.");
+                    }
+                    else
+                    {
+                        if (con.Item2 != null)
+                        {
+                            MessageBox.Show(con.Item2.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtReceive.Text != "")
+                    txtReceive.Text += "\n";
+
+                if (pathCombo.SelectedItem.ToString() == "RS232")
+                {
+                    //CodeReader cr = new CodeReader();
+                    serialDelegate = cr.serialStart;
+                    var con = cr.serialStart(serialPort);
+                    if (con.ex != null)
+                    {
+                        MessageBox.Show(con.ex.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else if (pathCombo.SelectedItem.ToString() == "TCP")
+                {
+                    // Establish a TCP connection to trigger scan
+                    tcpReceiveDelegate = cr.tcpStart;
+                    var con = tcpReceiveDelegate(serverIP, serverPort);
+                    if (con.Item1 != null)
+                    {
+                        foreach (char c in con.Item1)
+                        {
+                            if (c == ';')
+                                txtReceive.Text += "\nAngle: ";
+                            else
+                                txtReceive.Text += c;
+                        }
+                    }
+                    else
+                    {
+                        if (con.Item2 != null)
+                        {
+                            MessageBox.Show(con.Item2.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+                txtReceive.ScrollToLine(txtReceive.LineCount - 1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            //CodeReader cr = new CodeReader();
+            serialReceiveDelegate = cr.serialReceive;
+            var con = serialReceiveDelegate(serialPort);
+            if (con.Item1 != null)
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    foreach (char c in con.Item1)
+                    {
+                        if (c == ';')
+                            txtReceive.Text += "\nAngle: ";
+                        else
+                            txtReceive.Text += c;
+                    }
+
+                    //txtReceive.Text += myans;
+                    txtReceive.ScrollToLine(txtReceive.LineCount - 1);
+                }));
+            }
+            
+        }
+
+        private void pathCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (pathCombo.SelectedItem.ToString() == "RS232")
+            {
+                string[] ports = SerialPort.GetPortNames();
+                if (ports.Length > 0)
+                {
+                    portCombo.ItemsSource = ports;
+                    portCombo.SelectedIndex = 0;
+                }
+                else
+                {
+                    portCombo.Items.Add("No Port");
+                    portCombo.SelectedIndex = 0;
+                }
+            }
+            else if (pathCombo.SelectedItem.ToString() == "TCP")
+            {
+                portCombo.SelectedItem = -1;
+                portCombo.ItemsSource = null;
+            }
+        }
+
+    }
+}
---
+++ SmartCodeReader/CodeReader.cs
@@ -0,0 +1,172 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.IO.Ports;
+
+namespace SmartCodeReader
+{
+    public class CodeReader
+    {
+        public (bool result, Exception? ex) tcpConnect(string serverIP,int commandPort)
+        {
+            try
+            {
+                TcpClient tcpClient = new TcpClient(serverIP, commandPort);
+                    if (tcpClient.Connected)
+                    {
+                        string dataToSend = "<Set,Acq,1>";
+                        byte[] sendData = Encoding.ASCII.GetBytes(dataToSend);
+                        NetworkStream stream = tcpClient.GetStream();
+                        stream.Write(sendData, 0, sendData.Length);
+                        return (true, null);
+                    }
+                    return (false, null);
+                
+            }
+            catch (Exception ex)
+            {
+                return (false, ex);
+            }
+        }
+
+        public (bool result, Exception? ex) tcpDisconnect(string serverIP, int commandPort)
+        {
+            try
+            {
+                // Establish a TCP connection to stop acquisition
+                TcpClient tcpClient = new TcpClient(serverIP, commandPort);
+                if (tcpClient.Connected)
+                {
+                    string dataToSend = "<Set,Acq,0>";
+                    byte[] sendData = Encoding.ASCII.GetBytes(dataToSend);
+                    NetworkStream stream = tcpClient.GetStream();
+                    stream.Write(sendData, 0, sendData.Length);
+                    return (true, null);
+                }
+                return (false, null);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex);
+            }
+        }
+
+        public (string? result, Exception? ex) tcpStart(string serverIP, int serverPort)
+        {
+            string ans = "";
+            try
+            {
+                TcpClient tcpClient2 = new TcpClient(serverIP, serverPort);
+                if (tcpClient2.Connected)
+                {
+                    // Send data to the server
+                    string dataToSend = "start";
+                    byte[] sendData = Encoding.ASCII.GetBytes(dataToSend);
+                    NetworkStream stream = tcpClient2.GetStream();
+                    stream.Write(sendData, 0, sendData.Length);
+
+                    // Receive data from the server
+                    byte[] receiveData = new byte[1024];
+                    int bytesRead = stream.Read(receiveData, 0, receiveData.Length);
+                    string receivedData = Encoding.ASCII.GetString(receiveData, 0, bytesRead);
+                    //txtReceive.Text += receivedData;
+                    foreach (char c in receivedData)
+                    {
+                        if (c == ';')
+                            ans += "\nAngle: ";
+                        else
+                            ans += c;
+                    }
+                    return (ans, null);
+                }
+                else
+                {
+                    return (null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                return (null, ex);
+            }
+
+        }
+
+        public (bool result, Exception? ex) serialConnect(SerialPort serialPort)
+        {
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                }
+                serialPort.Open();
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Write("<Set,Acq,1>");
+                    return (true, null);
+                }
+                return (false, null);
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex);
+            }
+        }
+
+        public (bool result, Exception? ex) serialDisconnect(SerialPort serialPort)
+        {
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Write("<Set,Acq,0>");
+                    serialPort.Close();
+                    return (true, null);
+                }
+                return (false, null);
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex);
+            }
+        }
+
+        public (bool result, Exception? ex) serialStart(SerialPort serialPort)
+        {
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Write("start");
+                    return (true, null);
+                }
+                return (false, null);
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex);
+            }
+        }
+
+        public (string? result, Exception? ex) serialReceive(SerialPort serialPort)
+        {
+            try
+            {
+                if (serialPort.BytesToRead > 0)
+                {
+                    byte[] data = new byte[serialPort.BytesToRead];
+                    var ans = serialPort.Read(data, 0, data.Length);
+                    string myans = Encoding.ASCII.GetString(data);
+                    return (myans, null);
+                }
+                return (null, null);
+            }
+            catch (Exception ex)
+            {
+                return (null, ex);
+            }
+        }
+    }
+}
